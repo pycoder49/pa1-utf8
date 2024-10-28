efd176eebc72dfde77a2977c77ada173972202a2
Aryan Ahuja <75223447+pycoder49@users.noreply.github.com>
2024-10-28 17:32:16 +0000

    initial pa1 working again

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 
-int32_t isascii(char str[]){
+int32_t is_ascii(char str[]){
     int32_t index = 0;
     while(str[index] != '\0'){
         if (!(0 < str[index] && str[index] < 128)){
@@ -57,10 +57,10 @@ int32_t utf8_strlen(char str[]){
 int32_t codepoint_index_to_byte_index(char str[], int32_t cpi){
     int byte_index = 0;
     int str_index = 0;
-    while(str[str_index] != '\0' && str_index <= cpi){
-        int32_t width = width_from_start_byte(str[str_index]);
+    while (str[str_index] != '\0' && str_index < cpi) {
+        int32_t width = width_from_start_byte(str[byte_index]);
         byte_index += width;
-        str_index += width;
+        str_index++;
     }
     return byte_index;
 }
@@ -74,12 +74,48 @@ void utf8_substring(char str[], int32_t cpi_start, int32_t cpi_end, char result[
     }
 }
 
+int32_t codepoint_at(char str[], int32_t cpi){
+    int start_index = codepoint_index_to_byte_index(str, cpi);
+    int32_t answer = 0;
+    int num_bytes = 0;
+
+    if ((str[start_index] & 0b10000000) == 0b00000000) {
+        return str[start_index];
+    } else if ((str[start_index] & 0b11100000) == 0b11000000) {
+        answer = str[start_index] & 0b00011111;
+        num_bytes = 2;
+    } else if ((str[start_index] & 0b11110000) == 0b11100000) {
+        answer = str[start_index] & 0b00001111;
+        num_bytes = 3;
+    } else if ((str[start_index] & 0b11111000) == 0b11110000) {
+        answer = str[start_index] & 0b00000111;
+        num_bytes = 4;
+    }
+
+    for (int i = 1; i < num_bytes; i++) {
+        answer = (answer << 6) | (str[start_index + i] & 0b00111111);
+    }
+
+    return answer;
+}
+
+char is_animal_emoji_at(char str[], int32_t cpi){
+    int32_t codepoint = codepoint_at(str, cpi);
+
+    if ((codepoint >= 0x1F400 && codepoint <= 0x1F43F) || 
+        (codepoint >= 0x1F980 && codepoint <= 0x1F9A7)) {
+        return 1;
+    }
+
+    return 0;
+}
+
 
 int main(){
     printf("----------\n");
     
     char string[] = "Hello!";
-    printf("is ascii: %d\n", isascii(string));
+    printf("is ascii: %d\n", is_ascii(string));
     printf("----------\n");
 
     int32_t ret = 0;
@@ -101,9 +137,13 @@ int main(){
     printf("----------\n");
 
     char result[17];
-    // utf8_substring("🦀🦮🦮🦀🦀🦮🦮", 3, 7, result);
-    // printf("String: %s\nSubstring: %s\n", "🦀🦮🦮🦀🦀🦮🦮", result);
-    // printf("----------\n");
+    utf8_substring("🦀🦮🦮🦀🦀🦮🦮", 3, 7, result);
+    printf("String: %s\nSubstring: %s\n", "🦀🦮🦮🦀🦀🦮🦮", result);
+    printf("----------\n");
+
+    char strrr[] = "Joséph";
+    printf("%d\n", codepoint_at(strrr, 3));
+    printf("----------\n");
 
     return 0;
 }
